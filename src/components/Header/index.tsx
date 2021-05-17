74b1b50705062fe8945dad9d2e5cf99cdccdd135
antonio-estevam <jhonnyimmbe@gmail.com>
2021-05-17 14:45:52 -0300

    00

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,23 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import style from  './styles.module.scss';
+
 export function Header() {
+    const currentData = format(new Date(),'EEEEEE, d MMMM',{
+        locale: ptBR,
+    });
+
     return(
-        <header>
-            
+        <header className = {style.haderContainer}>
+            <img src="/logo.svg" alt="Podecaster"/>
+
+            <p>O melhor para voçê ouvir, sempre</p>
+
+            <span>{currentData}</span>
         </header>
     );
 }
 
-// 39:56
+
+//49:35
