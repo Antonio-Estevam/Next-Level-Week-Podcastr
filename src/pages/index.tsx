b6b2aa4fa08fbde00452bfccf78c01b2d5b2b739
Antonio Estevam da Silva Filho(Jhonny) <47607852+Jhonny3x4@users.noreply.github.com>
2021-06-09 18:53:53 -0300

    create latest episodes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,68 @@
 import { GetStaticProps } from 'next';
+import Image from 'next/image';
 import { format, parseISO} from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
+import stles from './home.module.scss';
+
 type Episodes = {
   id: string;
-  title:string;
-  membrs: string;
-  published: string;
+  title: string;
+  thumbnail: string;
+  description: string;
+  members: string; 
+  duration: string; 
+  durationAsString: string; 
+  url: string; 
+  publishedAt: string;
 }
 
 
 type HomeProps = {
-  episodes: Array< Episodes >
+  latestEpisodes: Episodes[];
+  allEpisodes: Episodes[];
 } 
 
-export default function Home(props: HomeProps) {
-  
+export default function Home({ latestEpisodes,allEpisodes }: HomeProps) {  
   return ( 
-    <div>
-      <h1> index </h1>
-      <p> {JSON.stringify(props.episodes)} </p>
+    <div className= {stles.homepage}>
+      <section className={stles.latestEpisodes}>
+        <h2> Últimos lançamentos</h2>
+
+        <ul>
+          { latestEpisodes.map(episode => {
+            return(
+              <li key={episode.id}>
+                <Image 
+                  width={192} 
+                  height={192} 
+                  src={episode.thumbnail} 
+                  alt={episode.title}
+                  objectFit="cover" 
+                />
+
+                <div className={stles.episodeDetails}>
+                  <a href="">{episode.title}</a>
+                  <p>{ episode.members }</p>
+                  <span>{ episode.publishedAt }</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+
+                <button type="button">
+                  <img src="/play-green.svg" alt="Tocar episódio" />
+                </button>
+
+              </li>
+            )            
+          })}
+        </ul>
+      </section>
+
+      <section className={stles.allEpisodes}>
+
+      </section>
     </div>
    )
 }
@@ -49,11 +90,16 @@ export  const  getStaticProps: GetStaticProps = async () => {
     };
   })
 
+const latestEpisodes = episodes.slice(0,2);
+const allEpisodes = episodes.slice(2,episodes.length);
+
   return{
     props: {
-      episodes,
+      latestEpisodes,
+      allEpisodes,
     },
     revalidate: 60 * 60 * 8,
   }
 }
 
+ 
