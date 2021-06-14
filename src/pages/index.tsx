a1f577364b4b6b78616a69b41e7bbdc91b8c27ff
Antonio Estevam da Silva Filho(Jhonny) <47607852+Jhonny3x4@users.noreply.github.com>
2021-06-14 17:38:49 -0300

    create pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
 import { format, parseISO} from 'date-fns';
+import Link from 'next/link';
 import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
@@ -44,7 +45,9 @@ export default function Home({ latestEpisodes,allEpisodes }: HomeProps) {
                 />
 
                 <div className={stles.episodeDetails}>
-                  <a href="">{episode.title}</a>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
                   <p>{ episode.members }</p>
                   <span>{ episode.publishedAt }</span>
                   <span>{episode.durationAsString}</span>
@@ -66,12 +69,14 @@ export default function Home({ latestEpisodes,allEpisodes }: HomeProps) {
           <table cellSpacing={0}>
 
             <thead>
-              <th></th>
-              <th>Podcast</th>
-              <th>Integrantes</th>
-              <th>Data</th>
-              <th>Duração</th>
-              <th></th>              
+              <tr>
+                <th></th>
+                <th>Podcast</th>
+                <th>Integrantes</th>
+                <th>Data</th>
+                <th>Duração</th>
+                <th></th> 
+              </tr>             
             </thead>
             <tbody>
               {allEpisodes.map(episode => {
@@ -88,7 +93,9 @@ export default function Home({ latestEpisodes,allEpisodes }: HomeProps) {
                     </td>
 
                     <td>
-                     <a href="">{episode.title}</a>
+                      <Link href={`/episodes/${episode.id}`}>
+                        <a>{episode.title}</a>
+                      </Link>
                     </td>
 
                     <td>
